d4de48462ae217148679a5b0a3042b0ec92d799e
Soraya <icuestiona@gmail.com>
2022-10-15 16:19:57 +0200

    Added Dictionary.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Welcome to the dictionary</h1>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Soraya Carvajal</small>
+        </footer>
+      </div>
     </div>
   );
 }
